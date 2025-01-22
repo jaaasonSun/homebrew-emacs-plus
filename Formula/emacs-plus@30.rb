f70413b8fb6e2e0ca5e9b02e012abce222428bb6
sunjingyuan.sjy <sunjingyuan.sjy@alibaba-inc.com>
2025-01-22 14:55:38 +0800

    Fixed alpha-background for emacs-30 and add blur

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -107,7 +107,8 @@ class EmacsPlusAT30 < EmacsBase
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "9eb3ce80640025bff96ebaeb5893430116368d6349f4eb0cb4ef8b3d58477db6"
   local_patch "round-undecorated-frame", sha: "7451f80f559840e54e6a052e55d1100778abc55f98f1d0c038a24e25773f2874"
-  local_patch "alpha-background", sha: "922d9c5cd7deebd16773d354150faa8a5e69d998651cb2e956d9ed600232b4bc" 
+  local_patch "alpha-background", sha: "e5367aa56f7f48977827164226dddee5928699522fd64fdd7836325fb1c1a639"
+  local_patch "blur", sha: "f9c94861fc84620d97077c68f42bb2b2b1d25af75cf3a71b87c6ccf32a462f21"
 
   #
   # Install
